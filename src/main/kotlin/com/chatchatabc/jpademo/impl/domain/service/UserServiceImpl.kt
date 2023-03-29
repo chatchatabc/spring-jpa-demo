58beb257a567d748bf850e22190b7a39a6911265
Raphile14 <rllddalangin@gmail.com>
2023-03-29 10:58:45 +0800

    feat: created initial UserServiceImpl.kt

---
+++ src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/UserServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.chatchatabc.jpademo.impl.domain.service
+
+import com.chatchatabc.jpademo.domain.service.UserService
+import org.springframework.stereotype.Service
+
+@Service
+class UserServiceImpl : UserService {
+}
