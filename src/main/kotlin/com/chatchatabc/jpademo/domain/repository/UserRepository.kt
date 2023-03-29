026cd767695a460892820b72ba4c304e9621efc9
Raphile14 <rllddalangin@gmail.com>
2023-03-29 13:48:04 +0800

    feat: created findByUsername repository function

--- src/main/kotlin/com/chatchatabc/jpademo/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/domain/repository/UserRepository.kt
@@ -3,7 +3,13 @@ package com.chatchatabc.jpademo.domain.repository
 import com.chatchatabc.jpademo.domain.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, String> {
+
+    /**
+     * Find user by username
+     */
+    fun findByUsername(username: String): Optional<User>
 }
