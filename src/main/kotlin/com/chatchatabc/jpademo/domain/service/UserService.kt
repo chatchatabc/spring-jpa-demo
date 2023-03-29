bb9e00b51a22c526193859935cda182001136b54
Raphile14 <rllddalangin@gmail.com>
2023-03-29 11:31:07 +0800

    feat: created corresponding services for user registration

--- src/main/kotlin/com/chatchatabc/jpademo/domain/service/UserService.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/domain/service/UserService.kt
@@ -1,7 +1,14 @@
 package com.chatchatabc.jpademo.domain.service
 
+import com.chatchatabc.jpademo.application.dto.user.UserRegisterRequest
+import com.chatchatabc.jpademo.domain.model.User
 import org.springframework.stereotype.Service
 
 @Service
 interface UserService {
+
+    /**
+     * User Register
+     */
+    fun register(user: UserRegisterRequest): User
 }
--- src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/UserServiceImpl.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/UserServiceImpl.kt
@@ -1,8 +1,27 @@
 package com.chatchatabc.jpademo.impl.domain.service
 
+import com.chatchatabc.jpademo.application.dto.user.UserRegisterRequest
+import com.chatchatabc.jpademo.domain.model.User
+import com.chatchatabc.jpademo.domain.repository.UserRepository
 import com.chatchatabc.jpademo.domain.service.UserService
+import org.modelmapper.ModelMapper
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
-class UserServiceImpl : UserService {
+class UserServiceImpl(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) : UserService {
+    private val mapper = ModelMapper()
+
+    /**
+     * User Register
+     */
+    override fun register(user: UserRegisterRequest): User {
+        // Encrypt password
+        val newUser = mapper.map(user, User::class.java)
+        newUser.password = passwordEncoder.encode(user.password)
+        return userRepository.save(newUser)
+    }
 }
