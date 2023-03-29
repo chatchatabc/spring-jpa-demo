a25a5270527f7b01a36440dd8c75627ae91e49bb
Raphile14 <rllddalangin@gmail.com>
2023-03-29 10:16:52 +0800

    feat: created initial SecurityConfig.kt

---
+++ src/main/kotlin/com/chatchatabc/jpademo/infra/config/security/SecurityConfig.kt
@@ -0,0 +1,74 @@
+package com.chatchatabc.jpademo.infra.config.security
+
+import com.chatchatabc.jpademo.infra.config.security.filter.JwtRequestFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtRequestFilter: JwtRequestFilter
+) {
+    /**
+     * Configure security rules
+     */
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors().and().csrf().disable()
+            .authorizeHttpRequests {
+                // TODO: Add more security rules
+                it.anyRequest().permitAll()
+            }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .build()
+    }
+
+    /**
+     * Configure CORS
+     */
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        // TODO: Add this to application.properties
+        configuration.allowedOrigins = listOf("http://localhost:3000", "http://192.168.1.4:3000")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("authorization", "content-type", "x-auth-token", "X-Access-Token")
+        // Allow client to write to header
+        configuration.exposedHeaders = listOf("x-auth-token", "X-Access-Token")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    /**
+     * Authentication manager bean definition.
+     */
+    @Bean
+    fun authenticationManager(configuration: AuthenticationConfiguration): AuthenticationManager {
+        return configuration.authenticationManager
+    }
+
+    /**
+     * Password encoder bean definition.
+     */
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
