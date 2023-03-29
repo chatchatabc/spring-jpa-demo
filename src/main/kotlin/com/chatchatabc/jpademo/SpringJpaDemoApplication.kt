8a533d855e01bc5d16d60ad6db4355c274951d08
Raphile14 <rllddalangin@gmail.com>
2023-03-29 09:52:33 +0800

    initial commit

---
+++ src/main/kotlin/com/chatchatabc/jpademo/SpringJpaDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.chatchatabc.jpademo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJpaDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/chatchatabc/jpademo/SpringJpaDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chatchatabc.jpademo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJpaDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
