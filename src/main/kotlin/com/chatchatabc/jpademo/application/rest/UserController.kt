ec0fe749ea773f1ec61d1bb15b550bb049aafa46
Raphile14 <rllddalangin@gmail.com>
2023-03-29 14:29:39 +0800

    feat: implemented view profile of other user

--- src/main/kotlin/com/chatchatabc/jpademo/application/rest/UserController.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/application/rest/UserController.kt
@@ -7,6 +7,7 @@ import com.chatchatabc.jpademo.domain.repository.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,6 +25,24 @@ class UserController(
             // Get id from security context
             val principal = SecurityContextHolder.getContext().authentication.principal as User
             val user = userRepository.findById(principal.id)
+            ResponseEntity.ok(UserProfileResponse(user.get(), null))
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(UserProfileResponse(null, ErrorContent("User Profile Error", e.message)))
+        }
+    }
+
+    /**
+     * View profile of other user
+     */
+    @GetMapping("/profile/view/{username}")
+    fun viewProfile(
+        @PathVariable username: String
+    ) : ResponseEntity<UserProfileResponse> {
+        return try {
+            val user = userRepository.findByUsername(username)
+            if (user.isEmpty) {
+                throw Exception("User not found")
+            }
             ResponseEntity.ok(UserProfileResponse(user.get(), null))
         } catch (e: Exception) {
             ResponseEntity.badRequest().body(UserProfileResponse(null, ErrorContent("User Profile Error", e.message)))
