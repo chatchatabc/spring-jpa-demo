1cd97e8f61f6bd192ca5471f631183e4d9d8ccdb
Raphile14 <rllddalangin@gmail.com>
2023-03-29 14:10:41 +0800

    feat: created UserController.kt to handle /user api calls

---
+++ src/main/kotlin/com/chatchatabc/jpademo/application/rest/UserController.kt
@@ -0,0 +1,40 @@
+package com.chatchatabc.jpademo.application.rest
+
+import com.chatchatabc.jpademo.application.dto.ErrorContent
+import com.chatchatabc.jpademo.application.dto.user.UserProfileResponse
+import com.chatchatabc.jpademo.domain.model.User
+import com.chatchatabc.jpademo.domain.repository.UserRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(
+    private val userRepository: UserRepository
+) {
+    /**
+     * User Profile
+     */
+    @GetMapping("/profile")
+    fun profile(): ResponseEntity<UserProfileResponse> {
+        return try {
+            // Get id from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as User
+            val user = userRepository.findById(principal.id)
+            ResponseEntity.ok(UserProfileResponse(user.get(), null))
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(UserProfileResponse(null, ErrorContent("User Profile Error", e.message)))
+        }
+    }
+
+    /**
+     * TODO: Update User Profile
+     */
+
+    /**
+     * TODO: Update User Password
+     */
+}
