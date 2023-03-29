dd2e33d32b02fe2bb362dbad27e6db7592b6e7d3
Raphile14 <rllddalangin@gmail.com>
2023-03-29 14:46:39 +0800

    feat: implemented update user profile api

--- src/main/kotlin/com/chatchatabc/jpademo/application/rest/UserController.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/application/rest/UserController.kt
@@ -2,19 +2,20 @@ package com.chatchatabc.jpademo.application.rest
 
 import com.chatchatabc.jpademo.application.dto.ErrorContent
 import com.chatchatabc.jpademo.application.dto.user.UserProfileResponse
+import com.chatchatabc.jpademo.application.dto.user.UserProfileUpdateRequest
 import com.chatchatabc.jpademo.domain.model.User
 import com.chatchatabc.jpademo.domain.repository.UserRepository
+import com.chatchatabc.jpademo.domain.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/user")
 class UserController(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val userService: UserService
 ) {
     /**
      * User Profile
@@ -37,7 +38,7 @@ class UserController(
     @GetMapping("/profile/view/{username}")
     fun viewProfile(
         @PathVariable username: String
-    ) : ResponseEntity<UserProfileResponse> {
+    ): ResponseEntity<UserProfileResponse> {
         return try {
             val user = userRepository.findByUsername(username)
             if (user.isEmpty) {
@@ -50,8 +51,22 @@ class UserController(
     }
 
     /**
-     * TODO: Update User Profile
+     * Update User Profile
      */
+    @Transactional
+    @PutMapping("/profile/update")
+    fun updateProfile(
+        @RequestBody user: UserProfileUpdateRequest
+    ): ResponseEntity<UserProfileResponse> {
+        return try {
+            // Get id from security context
+            val principal = SecurityContextHolder.getContext().authentication.principal as User
+            val user = userService.update(principal.id, user)
+            ResponseEntity.ok(UserProfileResponse(user, null))
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().body(UserProfileResponse(null, ErrorContent("User Profile Error", e.message)))
+        }
+    }
 
     /**
      * TODO: Update User Password
