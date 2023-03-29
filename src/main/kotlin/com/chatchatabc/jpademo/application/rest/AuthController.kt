de66ca426263511b526d7c78dfe2ee1181670d33
Raphile14 <rllddalangin@gmail.com>
2023-03-29 11:32:13 +0800

    feat: created user registration for AuthController.kt

--- src/main/kotlin/com/chatchatabc/jpademo/application/rest/AuthController.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/application/rest/AuthController.kt
@@ -1,16 +1,46 @@
 package com.chatchatabc.jpademo.application.rest
 
+import com.chatchatabc.jpademo.application.dto.ErrorContent
+import com.chatchatabc.jpademo.application.dto.user.UserRegisterRequest
+import com.chatchatabc.jpademo.application.dto.user.UserRegisterResponse
+import com.chatchatabc.jpademo.domain.service.UserService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/auth")
-class AuthController {
+class AuthController(
+    private val userService: UserService
+) {
     /**
      * TODO: User Login
      */
 
     /**
-     * TODO: User Registration
+     * User Registration
+     */
+    @PostMapping("/register")
+    fun register(
+        @RequestBody userRegisterRequest: UserRegisterRequest
+    ): ResponseEntity<UserRegisterResponse> {
+        return try {
+            val newUser = userService.register(userRegisterRequest)
+            return ResponseEntity.ok(UserRegisterResponse(newUser, null))
+        } catch (e: Exception) {
+            // TODO: Catch appropriate exception message
+            ResponseEntity.badRequest()
+                .body(UserRegisterResponse(null, ErrorContent("User Registration Error", e.message)))
+        }
+    }
+
+    /**
+     * TODO: Check if username is available
+     */
+
+    /**
+     * TODO: Check if email is available
      */
 }
