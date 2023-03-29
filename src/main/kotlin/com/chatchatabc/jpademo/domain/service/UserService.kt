b200c7dab54fef3b004625e8f331951ce3ca0c0f
Raphile14 <rllddalangin@gmail.com>
2023-03-29 13:48:19 +0800

    feat: implemented login service logic

--- src/main/kotlin/com/chatchatabc/jpademo/domain/service/UserService.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/domain/service/UserService.kt
@@ -2,10 +2,11 @@ package com.chatchatabc.jpademo.domain.service
 
 import com.chatchatabc.jpademo.application.dto.user.UserRegisterRequest
 import com.chatchatabc.jpademo.domain.model.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-interface UserService {
+interface UserService : UserDetailsService {
 
     /**
      * User Register
--- src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/UserServiceImpl.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/UserServiceImpl.kt
@@ -5,13 +5,15 @@ import com.chatchatabc.jpademo.domain.model.User
 import com.chatchatabc.jpademo.domain.repository.UserRepository
 import com.chatchatabc.jpademo.domain.service.UserService
 import org.modelmapper.ModelMapper
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
 ) : UserService {
     private val mapper = ModelMapper()
 
@@ -23,5 +25,24 @@ class UserServiceImpl(
         val newUser = mapper.map(user, User::class.java)
         newUser.password = passwordEncoder.encode(user.password)
         return userRepository.save(newUser)
+    }
+
+    /**
+     * User Login
+     */
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user: Optional<User> = userRepository.findByUsername(username)
+        if (user.isEmpty) {
+            throw Exception("User not found")
+        }
+        return org.springframework.security.core.userdetails.User(
+            user.get().username,
+            user.get().password,
+            user.get().isEnabled,
+            user.get().isAccountNonExpired,
+            user.get().isCredentialsNonExpired,
+            user.get().isAccountNonLocked,
+            user.get().authorities
+        )
     }
 }
