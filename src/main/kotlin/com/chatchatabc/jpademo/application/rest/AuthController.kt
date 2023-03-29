2578cc2ff7a4525820638418444f50c2b2d6abb7
Raphile14 <rllddalangin@gmail.com>
2023-03-29 10:59:05 +0800

    feat: created initial AuthController.kt draft

---
+++ src/main/kotlin/com/chatchatabc/jpademo/application/rest/AuthController.kt
@@ -0,0 +1,16 @@
+package com.chatchatabc.jpademo.application.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthController {
+    /**
+     * TODO: User Login
+     */
+
+    /**
+     * TODO: User Registration
+     */
+}
