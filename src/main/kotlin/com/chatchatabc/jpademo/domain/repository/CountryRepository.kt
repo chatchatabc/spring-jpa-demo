f5160a7e641c4db172c64762568768bbfceeb24d
Raphile14 <rllddalangin@gmail.com>
2023-03-30 11:27:26 +0800

    feat: created CountryRepository.kt

---
+++ src/main/kotlin/com/chatchatabc/jpademo/domain/repository/CountryRepository.kt
@@ -0,0 +1,9 @@
+package com.chatchatabc.jpademo.domain.repository
+
+import com.chatchatabc.jpademo.domain.model.Country
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CountryRepository : JpaRepository<Country, String> {
+}
