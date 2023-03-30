3e1076f5ed882adb5c3ed0edff317003c260d79b
Raphile14 <rllddalangin@gmail.com>
2023-03-30 11:40:40 +0800

    feat: created update service function for Country

--- src/main/kotlin/com/chatchatabc/jpademo/domain/service/CountryService.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/domain/service/CountryService.kt
@@ -1,6 +1,7 @@
 package com.chatchatabc.jpademo.domain.service
 
 import com.chatchatabc.jpademo.application.dto.country.CountryCreateRequest
+import com.chatchatabc.jpademo.application.dto.country.CountryUpdateRequest
 import com.chatchatabc.jpademo.domain.model.Country
 import org.springframework.stereotype.Service
 
@@ -10,4 +11,9 @@ interface CountryService {
      * Create Country
      */
     fun create(country: CountryCreateRequest): Country
+
+    /**
+     * Update Country
+     */
+    fun update(countryId: String, request: CountryUpdateRequest): Country
 }
--- src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/CountryServiceImpl.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/impl/domain/service/CountryServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.chatchatabc.jpademo.impl.domain.service
 
 import com.chatchatabc.jpademo.application.dto.country.CountryCreateRequest
+import com.chatchatabc.jpademo.application.dto.country.CountryUpdateRequest
 import com.chatchatabc.jpademo.domain.model.Country
 import com.chatchatabc.jpademo.domain.repository.CountryRepository
 import com.chatchatabc.jpademo.domain.service.CountryService
@@ -19,5 +20,22 @@ class CountryServiceImpl (
     override fun create(country: CountryCreateRequest): Country {
         val newCountry = mapper.map(country, Country::class.java)
         return countryRepository.save(newCountry)
+    }
+
+    /**
+     * Update Country
+     */
+    override fun update(countryId: String, request: CountryUpdateRequest): Country {
+        val queriedCountry = countryRepository.findById(countryId)
+        if (queriedCountry.isEmpty) {
+            throw Exception("Country not found")
+        }
+
+        // Update fields
+        queriedCountry.get().apply {
+            name = request.name ?: name
+        }
+
+        return countryRepository.save(queriedCountry.get())
     }
 }
