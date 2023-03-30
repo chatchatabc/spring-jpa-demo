1643e5f126273695baed2d2501941f4a017d1516
Raphile14 <rllddalangin@gmail.com>
2023-03-30 11:54:42 +0800

    feat: created CountryAssign.kt

---
+++ src/main/kotlin/com/chatchatabc/jpademo/application/dto/country/CountryAssign.kt
@@ -0,0 +1,14 @@
+package com.chatchatabc.jpademo.application.dto.country
+
+import com.chatchatabc.jpademo.application.dto.ErrorContent
+import com.chatchatabc.jpademo.domain.model.User
+
+data class CountryAssignRequest(
+    val userId: String,
+    val countryId: String
+)
+
+data class CountryAssignResponse(
+    val user: User?,
+    val error: ErrorContent?
+)
