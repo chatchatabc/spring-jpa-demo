076b9c3974101b917637da2f140a5960e6eb36f4
Raphile14 <rllddalangin@gmail.com>
2023-03-30 13:35:40 +0800

    feat: created findUsersByCountry

--- src/main/kotlin/com/chatchatabc/jpademo/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/chatchatabc/jpademo/domain/repository/UserRepository.kt
@@ -1,6 +1,9 @@
 package com.chatchatabc.jpademo.domain.repository
 
+import com.chatchatabc.jpademo.domain.model.Country
 import com.chatchatabc.jpademo.domain.model.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -12,4 +15,9 @@ interface UserRepository : JpaRepository<User, String> {
      * Find user by username
      */
     fun findByUsername(username: String): Optional<User>
+
+    /**
+     * Find user by country
+     */
+    fun findUsersByCountry(country: Country, pageable: Pageable): Page<User>
 }
